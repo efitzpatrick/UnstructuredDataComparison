9bf1c68ac507fd4de8b493dc088ab452c392279f
Ryan Paddock <rmp125@case.edu>
2020-04-02 13:56:43 -0400

    Added all the interface methods to MicroAggregation

--- src/main/java/com/dataprivacy/app/MicroAggregation.java
+++ src/main/java/com/dataprivacy/app/MicroAggregation.java
@@ -1,11 +1,31 @@
 package com.dataprivacy.app;
 
-public class MicroAggregation extends Algorithms {
+import java.time.Duration;
+
+public class MicroAggregation implements Algorithms {
 
     public void run()
     {
 
 
+    }
+
+    public void setData()
+    {
+
+    }
+
+    public boolean checkKAnonymous()
+    {
+        return false;
+    }
+    public float checkDataloss()
+    {
+        return 0;
+    }
+    public Duration checkRuntime()
+    {
+        return null;
     }
 
 }
