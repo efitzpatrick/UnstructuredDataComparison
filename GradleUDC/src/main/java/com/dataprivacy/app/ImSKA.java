769fed88ca36cb882793ae225d27c30025cbc779
dianabooklover <diana.zavela@gmail.com>
2020-04-04 16:02:44 -0400

    Adding method skeleton  to ImSKA

---
+++ GradleUDC/src/main/java/com/dataprivacy/app/ImSKA.java
@@ -0,0 +1,38 @@
+package com.dataprivacy.app;
+
+import java.time.Duration;
+
+public class ImSKA implements Algorithms {
+
+    public void run()
+    {
+
+    }
+
+    public T[][] setData()
+    {
+        return null;
+    }
+
+    public void aggregate()
+    {
+
+    }
+
+    //public T[][][]
+
+    public boolean checkKAnonymous()
+    {
+        return false;
+    }
+
+    public float checkDataloss()
+    {
+        return 0;
+    }
+
+    public Duration checkRuntime()
+    {
+        return null;
+    }
+}
