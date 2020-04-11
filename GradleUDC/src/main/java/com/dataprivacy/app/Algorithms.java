b575b20252338b1c101dfb3c613e142e3e2c9a6f
Ryan Paddock <rmp125@case.edu>
2020-04-10 22:16:59 -0400

    checkKAnonymous

--- GradleUDC/src/main/java/com/dataprivacy/app/Algorithms.java
+++ GradleUDC/src/main/java/com/dataprivacy/app/Algorithms.java
@@ -4,13 +4,30 @@ import java.time.Duration;
 
 public abstract class Algorithms {
     String[][] processedData;
-
+    boolean[] isSensitive;
     String[][] setData(){
         return null;
     }
     abstract void run();
     boolean checkKAnonymous(int k){
+        int anonCheck = 0;
+        for(int i = 0; i < processedData.length; i++){
+            for(int m = 0; m < processedData[i].length; i++) {
+                if (isSensitive[i]) {
+                    for (int j = i; j < processedData.length; i++) {
+                        if (processedData[i][m] ==processedData[j][m]){
+                            anonCheck++;
+                        }
+                    }
+                    if (anonCheck >= k) {
+                        return false;
+                    }
+                    anonCheck = 0;
+                }
+            }
+        }
         return true;
     }
+
     abstract float checkDataLoss();
 }
