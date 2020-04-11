490468fa3ae0f3e10acdaa439a0ac4df700e8c65
Ryan Paddock <rmp125@case.edu>
2020-04-11 16:25:33 -0400

    MicroAggregation

--- GradleUDC/src/main/java/com/dataprivacy/app/MicroAggregation.java
+++ GradleUDC/src/main/java/com/dataprivacy/app/MicroAggregation.java
@@ -1,5 +1,5 @@
 package com.dataprivacy.app;
-
+import java.util.Arrays;
 import java.time.Duration;
 
 public class MicroAggregation extends Algorithms {
@@ -10,54 +10,56 @@ public class MicroAggregation extends Algorithms {
 
     }
 
-    public String[][] aggregate (String[][] set, int k)
-    {
+    public String[][] aggregate (String[][] set, int k) {
+        int timerCounter = 0;
         if(firstAggBoolean){
             setStructure = new int[set.length];
             for(int x = 0; x < setStructure.length; x++){
-                setStructure[x] = x/4;
+                setStructure[x] = x/(16*k);
             }
         }
+        for(int x = 0; x < setStructure.length; x++){
+            setStructure[x] = x/8;
+        }
         for(int i = 0; i < set.length; i++) {
+            timerCounter = setStructure[i] - setStructure[i-1];
+            if(timerCounter != 0){
+                while(timerCounter == 0){
+                    timerCounter = setStructure[i] - setStructure[i-1];
 
+                }
+            }
         }
         return null;
     }
 
-    boolean microKAnon(int[] structure, String[][] data, int k){
+    boolean microKAnonymous(String[][] set, int k){
+        int[] tupleMarker = new int[set.length];
+        boolean isMatching = false;
         int anonCheck = 0;
-        boolean newGroup = false;
-        boolean restOfGroup = false;
-        int i = 0;
-        int j = 0;
-        int y = 0;
-        for(int m = 0; m < data.length; m++) {
-            while (!newGroup) {
-                for (int n = 0; n < data[i].length; n++) {
-                    while (!restOfGroup) {
-                        if (data[i][n] == data[j][n]) {
-                            anonCheck++;
+        for(int i = 0; i < set.length; i++){
+            if(tupleMarker[i] != 1) {
+                for (int j = 0; j < set.length; j++) {
+                    int n = 0;
+                    while (isMatching && n < set[i].length) {
+                        if (set[i][n] != set[j][n]) {
+                            isMatching = false;
                         }
-                        if (structure[j] != structure[i]) {
-                            restOfGroup = true;
-                        }
+                        n++;
                     }
-                    if(anonCheck < k){
-                        return false;
+                    if (isMatching) {
+                        anonCheck++;
+                        tupleMarker[i] = 1;
+                        tupleMarker[j] = 1;
                     }
-                    anonCheck = 0;
-                    restOfGroup = false;
+                    isMatching = true;
                 }
-                i++;
-                if(i != y){
-                    y = i;
-                    newGroup = true;
+                if(anonCheck < k){
+                    return false;
                 }
-
-                j = i;
-
+                anonCheck = 0;
             }
-            newGroup = false;
+
         }
         return true;
     }
