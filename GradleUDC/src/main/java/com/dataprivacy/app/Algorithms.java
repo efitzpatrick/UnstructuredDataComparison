f7bedfd05105dcd446ba4e30de72a3c7c11acc1b
ellie <efitzpatrickprogrammer@gmail.com>
2020-04-10 21:30:10 -0400

    Added Gradle files, did not include .gradle/ and build/ .

--- GradleUDC/src/main/java/com/dataprivacy/app/Algorithms.java
+++ GradleUDC/src/main/java/com/dataprivacy/app/Algorithms.java
@@ -2,10 +2,13 @@ package com.dataprivacy.app;
 import java.lang.reflect.Type;
 import java.time.Duration;
 
-public interface Algorithms {
-    String[][] setData();
-    void run();
-    boolean checkKAnonymous(int k);
-    float checkDataLoss();
-    Duration checkRuntime();
+public abstract class Algorithms {
+    String[][] setData(){
+        return null;
+    }
+    abstract void run();
+    boolean checkKAnonymous(int k){
+        return true;
+    }
+    abstract float checkDataLoss();
 }
