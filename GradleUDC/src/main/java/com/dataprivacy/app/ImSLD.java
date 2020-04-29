5e4cc397e54e0e32d0d490451bc1c1f53068d625
ellie <efitzpatrickprogrammer@gmail.com>
2020-04-29 01:15:51 -0400

    More minor fixes

--- GradleUDC/src/main/java/com/dataprivacy/app/ImSLD.java
+++ GradleUDC/src/main/java/com/dataprivacy/app/ImSLD.java
@@ -11,40 +11,40 @@ public class ImSLD extends HadoopAlgorithms {
     public void run(String path, int k) throws IOException {
         long sLDStartTime = System.nanoTime();
         setData(path);
-        try {
-            //rearrange columns in ascending order of numbers of unique values in it
-            // do the following over each column so make an array of length (number of columns)
-            //for each column{
-            Object[] a = new Object[14];
-            int[] numunique = new int[14];
-            for (int it = 0; it < 14; it++) {
-                pigServer.registerQuery("users = FOREACH data GENERATE $" + it);
-                pigServer.registerQuery("uniq_users = DISTINCT users");
-                pigServer.registerQuery("grouped_users = GROUP uniq_users ALL");
-                pigServer.registerQuery("uniq_user_count = FOREACH grouped_users GENERATE COUNT(uniq_users)");
-                numunique[it] = Integer.parseInt(("uniq_user_count"));
-            }
-            for (int cc = 0; cc < 15; cc++) {
-                int smallest = 0;
-                for (int j = cc + 1; j < 15; j++) {
-                    if (numunique[cc] > numunique[j]) {
-                        smallest = j;
-                    }
-                    pigServer.registerQuery("users = FOREACH user_data GENERATE $" + j);
-
-                }
-            }
-
-            //group by QID attribute: screenname
-            pigServer.registerQuery("order_by_data = ORDER data BY screenname DESC");
-
-            //create equivalence classes
-
-            //filter sensitive attributes from equivalence classes
-            pigServer.registerQuery("dropped_columns = FOREACH data GENERATE name, gender, location, foursqr_id");
-
-            //check if it is correct
-            long sLDEndTime = System.nanoTime();
+        //rearrange columns in ascending order of numbers of unique values in it
+        // do the following over each column so make an array of length (number of columns)
+        //for each column{
+        Object[] a = new Object[14];
+        int[] numunique = new int[14];
+        for (int it = 0; it < 14; it++) {
+            pigServer.registerQuery("users = FOREACH data GENERATE $" + it);
+            pigServer.registerQuery("uniq_users = DISTINCT users");
+            pigServer.registerQuery("grouped_users = GROUP uniq_users ALL");
+            pigServer.registerQuery("uniq_user_count = FOREACH grouped_users GENERATE COUNT(uniq_users)");
+            numunique[it] = Integer.parseInt(("uniq_user_count"));
         }
+        for (int cc = 0; cc < 15; cc++) {
+            int smallest = 0;
+            for (int j = cc + 1; j < 15; j++) {
+                if (numunique[cc] > numunique[j]) {
+                    smallest = j;
+                }
+                pigServer.registerQuery("users = FOREACH user_data GENERATE $" + j);
+
+            }
+        }
+
+        //group by QID attribute: screenname
+        pigServer.registerQuery("order_by_data = ORDER data BY screenname DESC");
+
+        //create equivalence classes
+
+        //filter sensitive attributes from equivalence classes
+        pigServer.registerQuery("dropped_columns = FOREACH data GENERATE name, gender, location, foursqr_id");
+
+        //check if it is correct
+        long sLDEndTime = System.nanoTime();
+
     }
 }
+
