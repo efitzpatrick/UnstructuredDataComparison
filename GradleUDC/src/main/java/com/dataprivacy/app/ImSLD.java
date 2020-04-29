e68fe981efeaf0d8dee528176f6bad958653f494
dianabooklover <diana.zavela@gmail.com>
2020-04-29 00:20:38 -0400

    import data in Algorithms, commented-out code for Im-SLD

--- GradleUDC/src/main/java/com/dataprivacy/app/ImSLD.java
+++ GradleUDC/src/main/java/com/dataprivacy/app/ImSLD.java
@@ -1,36 +1,63 @@
 package com.dataprivacy.app;
 
+import org.apache.pig.data.Tuple;
+
 import java.time.Duration;
+import java.util.Iterator;
 
 
 public class ImSLD extends HadoopAlgorithms {
 
-    public void run(String[][] data, int k)
-    {
+    public void run(int k) {
         long sLDStartTime = System.nanoTime();
         //rearrange columns in ascending order of numbers of unique values in it
         // do the following over each column so make an array of length (number of columns)
         //for each column{
-        /*users = FOREACH user_data GENERATE column name;
-        uniq_users = DISTINCT users;
-        grouped_users = GROUP uniq_users ALL;
-        uniq_user_count = FOREACH grouped_users GENERATE COUNT(uniq_users);
-        DUMP uniq_user_count; into array[column number]
+        Object[] a = new Object[14];
+        int[] numunique = new int[14];
+
+
+        try {
+            for(int it = 0; it<14; it++){
+                pigServer.registerQuery("users = FOREACH user_data GENERATE $"+it);
+                pigServer.registerQuery("uniq_users = DISTINCT users");
+                pigServer.registerQuery("grouped_users = GROUP uniq_users ALL");
+                pigServer.registerQuery("uniq_user_count = FOREACH grouped_users GENERATE COUNT(uniq_users)");
+                numunique[it] = pigServer.registerQuery("uniq_user_count");
+            }
+            for(int cc = 0; cc<15; cc++){
+                int smallest = 0;
+                for (int j = cc + 1; j < 15; j++)
+                {
+                    if (a[cc] > a[j])
+                    {
+                        smallest = j;
+                    }
+                }
+            }
         }
-        //create  a new database thingy
-        for 1:last column
-        iterate over array with unique number in each columns. pick the lowest. put that column into
-        the new database. it's now sorted!
-        */
 
-        //group by QID attributes
-        //name = get the name of the first column
-        //order_by_data = ORDER database BY name DESC;
 
-        //create equivalence classes
-        //filter sensitive attributes from equivalence classes
-        //check if it is correct
-        long sLDEndTime = System.nanoTime();
+
+            //create  a new database thingy
+           /* for 1:last column
+            iterate over array with unique number in each columns.pick the lowest.put that column into
+            the new database.it 's now sorted!
+            */
+
+
+            //group by QID attributes
+            //name = get the name of the first column
+            pigServer.registerQuery("order_by_data = ORDER (last database name) BY name DESC");
+
+            //create equivalence classes
+            //filter sensitive attributes from equivalence classes
+            //check if it is correct
+            long sLDEndTime = System.nanoTime();
+        }
+        catch (Exception e) {
+        e.printStackTrace();
+    }
     }
 
     public String[][] setData()
@@ -38,9 +65,4 @@ public class ImSLD extends HadoopAlgorithms {
         return null;
     }
 
-
-    @Override
-    public void run(int k) {
-
-    }
 }
